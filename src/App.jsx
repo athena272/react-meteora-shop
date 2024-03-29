a529c4f8c5a0bc1dca3b3d7074208b69291d06c8
athena272 <guilhermera272@gmail.com>
2024-03-29 17:05:36 -0300

    update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import Carrinho from "./pages/Carrinho";
 import PaginaErro from "./pages/PaginaErro";
-//Import bootstrap js and css
 
 import "./App.css";
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//Import bootstrap js and css
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'material-icons/iconfont/material-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App.jsx'
 import './index.css'
 
